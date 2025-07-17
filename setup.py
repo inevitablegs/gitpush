d1625633dae3f9eec5bfc0bc8727dd2f8bd2e98c
inevitablegs <sonawaneganu3101@gmail.com>
2025-07-17 15:09:06 +0530

    Build Sucessful

--- setup.py
+++ setup.py
@@ -2,21 +2,21 @@ from setuptools import setup, find_packages
 from pathlib import Path
 
 setup(
-    name="gitpush",
-    version="0.1.0",
+    name="gitpush-tool",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[],
     entry_points={
         "console_scripts": [
-            "gitpush=gitpush.cli:run"
+            "gitpush_tool=gitpush_tool.cli:run"
         ],
     },
     author="Ganesh Sonawane",
     author_email="sonawaneganu3101@example.com",
-    description="A simple CLI tool to push git commits with a single command.",
+    description="A CLI tool to simplify Git push operations with intelligent defaults and options.",
     long_description=Path("README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/gitpush",
+    url="https://github.com/yourusername/gitpush_tool",
     license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
